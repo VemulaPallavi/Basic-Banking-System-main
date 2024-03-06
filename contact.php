a28964a6cc383c0e429bfc1d1b8efbaca4499ef3
VEMULA PALLAVI <vemulapallavi25@gmail.com>
2024-03-06 17:07:29 +0530

    first commit

---
+++ contact.php
@@ -0,0 +1,62 @@
+</<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>BANKING SYSTEM</title>
+<link rel="stylesheet" href="style_about.css">
+  </head>
+
+  <body>
+<div class="navbar">
+<center>
+    <hr color="grey" width=70% size=1>
+  <h1> BASIC BANKING SYSTEM </h1>
+  <a href="index.php">Home</a>
+  <a href="sendmoney.php">Send Money</a>
+  <a href="customers.php">View All Customers</a>
+  <a href="transactions.php">Transactions</a>
+  <a class="active" href="contact.php">Contact Us</a>
+  <a href="about.php">About Us</a>
+
+      <hr color="grey" width=70% size=1>
+      </center>
+</div>
+
+<div class="container">
+<div class="header"> Welcome to Sparks Bank! </div>
+<img src="bank.png" height=65% width=30% alt="Welcome to the sparks bank!" style="padding: 5px; margin-right: 8vw; margin-top:8vh; float:right"> 
+</div>
+  <center>
+  <div class="contentbox">
+  
+    <h1> CONTACT US</h1>
+    <img src="phone.jpg" alt="Contact Us" width="50px" height="50px" style="
+  vertical-align: middle; border-radius: 50%;border-style: double; margin:2%;">
+    <h5> +91 6********* </h5>
+    <br>
+    
+    <img src="email.jpg" alt="Contact Us" width="50px" height="50px" 
+   style="vertical-align: middle; border-radius: 50%;border-style: double; margin:2%;">
+    <h5>  vemulapallavi@gmail.com </h5>
+    <br>
+    <img src="address.jpg" alt="Contact Us" width="50px" height="50px" style="
+  vertical-align: middle; border-radius: 50%;border-style: double; margin:2%;;"> 
+    <h5> Bangalore, India</h5>
+    <br>
+
+      <center>
+      <div class="mapouter"><div class="gmap_canvas"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7775.131143080342!2d77.58438634067313!3d12.999614649273159!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1649294a5637%3A0xb1f8b77e331512cf!2sBengaluru%20Palace!5e0!3m2!1sen!2sin!4v1709723617572!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div><style>.mapouter{position:relative;text-align:center;width:100%;height:400px;padding-left:10%;}.gmap_canvas {overflow:hidden;background:none!important;width:80%;height:400px;}.gmap_iframe {height:400px!important;}</style></div>       
+      </center>
+        <br> <br>
+  </div>
+  </center>
+
+<div class="pagebreak">
+</div>
+<div class="footer"> <center>
+Banking System ,Bangalore,India
+</center>
+</div>
+</center>
+  </body>
+</html>
